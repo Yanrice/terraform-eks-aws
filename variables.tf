966553ff06ee27894f469d076883d7ca6acb78c0
Yannick  Kalukuta <yannickkalukuta@gmail.com>
2025-08-18 03:27:27 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project for resource naming"
+  type        = string
+  default     = "my-eks-project"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.30"
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in the node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in the node group"
+  type        = number
+  default     = 3
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes in the node group"
+  type        = number
+  default     = 2
+}
+
+variable "node_instance_type" {
+  description = "Instance type for the EKS nodes"
+  type        = string
+  default     = "t3.medium"
+}
